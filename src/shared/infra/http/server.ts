5799d6ec67a84881b2998bfc4e88462d7b4b287e
Matheus Heck <matheus@conduite.tv>
2020-09-23 15:29:32 +0200

    change for DDD

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,15 +4,15 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
 
-import './database';
+import '@shared/infra/typeorm';
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
