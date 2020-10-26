7453ff1d09f889067582ef0134774250f2b32487
Matheus Heck <matheus@conduite.tv>
2020-10-26 20:37:28 +0100

    http routes Joi celebrate validation

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ForgottenPasswordController from '@modules/users/infra/http/controllers/ForgottenPasswordController';
 import ResetPasswordController from '@modules/users/infra/http/controllers/ResetPasswordController';
@@ -7,7 +8,25 @@ const passwordRouter = Router();
 const forgottenPasswordController = new ForgottenPasswordController();
 const resetPasswordController = new ResetPasswordController();
 
-passwordRouter.post('/forgot', forgottenPasswordController.create);
-passwordRouter.post('/reset', resetPasswordController.create);
+passwordRouter.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  forgottenPasswordController.create,
+);
+passwordRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  resetPasswordController.create,
+);
 
 export default passwordRouter;
